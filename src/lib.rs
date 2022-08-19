524bb4245e7b2e5157751c49fd873719f6f9cc63
Magamedrasul Ibragimov <curryrasul@gmail.com>
2022-08-19 14:42:06 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
-fn temp() {
-    let a = 1 + 1 + 1;
-}
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         let result = 2 + 2;
-        assert_eq!(result, 4);}
+        assert_eq!(result, 4);
+    }
 }
